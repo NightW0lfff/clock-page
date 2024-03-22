876d38ec6babbeb7601d2c0ba153a097766437ec
NightW0lfff <88699008+NightW0lfff@users.noreply.github.com>
2024-03-22 13:24:44 +1100

    Finalised adding analog clock

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 function App() {
-  const [time, setTime] = useState("");
+  const [time, setTime] = useState();
 
   useEffect(() => {
     const timer = setInterval(() => {
       setTime(new Date().toLocaleTimeString());
     }, 1000);
 
-    return clearInterval(timer);
+    return () => clearInterval(timer);
   }, []);
 
   return (
     <div className="App">
       <div className="container">
         <h1 className="time">{time}</h1>
+        <AnalogClock />
       </div>
+    </div>
+  );
+}
+
+function AnalogClock() {
+  const time = new Date();
+  const seconds = time.getSeconds();
+  const minutes = time.getMinutes();
+  const hours = time.getHours();
+
+  const secondHandRotation = 270 + seconds * 6;
+  const minuteHandRotation = 270 + minutes * 6 + seconds * 0.1;
+  const hourHandRotation = 270 + (hours % 12) * 30 + minutes * 0.5;
+  console.log(secondHandRotation);
+  return (
+    <div className="clock">
+      <div
+        className="hand hour"
+        style={{ transform: `rotate(${hourHandRotation}deg)` }}
+      ></div>
+      <div
+        className="hand minute"
+        style={{ transform: `rotate(${minuteHandRotation}deg)` }}
+      ></div>
+      <div
+        className="hand second"
+        style={{ transform: `rotate(${secondHandRotation}deg)` }}
+      ></div>
     </div>
   );
 }
